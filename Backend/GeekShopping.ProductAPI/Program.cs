241b271aebe9b10262a950e12c0fce6b04a6bf45
EduardoPPCaldas <eduardocaldas.dev@gmail.com>
2022-10-23 12:40:56 -0300

    Adding database connection and baseEntity model

---
+++ Backend/GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,36 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+#region Connection with database
+    var connectionString = builder.Configuration.GetConnectionString("MySQLConnection");
+    builder.Services.AddDbContext<MySQLContext>(options => 
+    {
+        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 5)));
+    });
+#endregion
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
